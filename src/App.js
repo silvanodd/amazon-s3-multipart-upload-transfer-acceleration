cb5b560d0d135cf62d9947a5443c3ca3463cd4be
Silvano <silvano@superroot.eu>
2023-07-19 11:45:10 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -48,15 +48,7 @@ function App() {
   }, [file])
 
   return (
-    <div >
-      // <div style={{ backgroundColor: "#e2e2e2", padding: "20px", margin: "10px"}}>    
-      //   <strong style={{display: "block"}}>Step 1 - Enter API URL --------------</strong><br/>
-      //   <input type="text" id="urlinput" style={{width: "50%"}} placeholder="https://psqxn67l81.execute-api.eu-west-2.amazonaws.com/prod/" 
-      //          onChange={(e) => {
-      //           setBaseUrl(e.target?.value)
-      //          }}
-      //   />
-      // </div>              
+    <div >         
       <div style={{ backgroundColor: "#e2e2e2", padding: "20px", margin: "10px"}}>
         <strong style={{display: "block"}}>Choose a file</strong><br/>
         <input type="file" id="fileinput" 
